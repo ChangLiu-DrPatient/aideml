8484251738ae206e574d4832d8a7df6905e44356
Yifu Cai <yifuc@dgx2.auton.cs.cmu.edu>
2025-04-21 16:28:24 -0400

    update logger level

--- aide/utils/config.py
+++ aide/utils/config.py
@@ -19,11 +19,7 @@ from . import tree_export
 from . import copytree, preproc_data, serialize
 
 shutup.mute_warnings()
-logging.basicConfig(
-    level="WARNING", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
-)
 logger = logging.getLogger("aide")
-logger.setLevel(logging.WARNING)
 
 
 """ these dataclasses are just for type hinting, the actual config is in config.yaml """
